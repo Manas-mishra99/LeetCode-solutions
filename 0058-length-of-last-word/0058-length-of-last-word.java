121b758903093e7f95bf7b6ccb314662554e5847
Manas Mishra <48373447+Manas-mishra99@users.noreply.github.com>
2024-08-29 00:48:17 +0530

    Time: 0 ms (100%), Space: 41.9 MB (11.65%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,9 +1,20 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        s=s.trim();
-        s=s.replaceAll("  "," ");
-        String []str= s.split(" ");
-        return str[str.length-1].length();
+        if (s.length() == 0) {
+            return 0;
+        }
+        String result = s.trim();
+        int count=0;
+        for (int i = result.length()-1; i >= 0; i--) {
+            char c = result.charAt(i);
+            if (c == ' '){
+                return count;
+            }
+            else {
+                count++;
+            }
+        }
+        return count;
         
     }
 }
